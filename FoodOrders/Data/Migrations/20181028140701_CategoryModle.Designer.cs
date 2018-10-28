2be9c30c815e6cd412566e2c7c1d549070c61b56
Abed3228 <Abed3228@gmail.com>
2018-10-28 16:08:54 +0200

    add category modle

--- FoodOrders/Data/Migrations/20181028140701_CategoryModle.Designer.cs
+++ FoodOrders/Data/Migrations/20181028140701_CategoryModle.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FoodOrders.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181028140701_CategoryModle")]
+    partial class CategoryModle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
