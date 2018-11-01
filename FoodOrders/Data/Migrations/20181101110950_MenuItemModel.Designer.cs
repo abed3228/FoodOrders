0cfea1181cb3bd0c1302522c90b641524a1e5c9d
Abed3228 <Abed3228@gmail.com>
2018-11-01 18:50:24 +0200

    Menu Item model and view and controller SD(for image in Meun item)

--- FoodOrders/Data/Migrations/20181101110950_MenuItemModel.Designer.cs
+++ FoodOrders/Data/Migrations/20181101110950_MenuItemModel.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FoodOrders.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181101110950_MenuItemModel")]
+    partial class MenuItemModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
