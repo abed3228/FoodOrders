87f681822b903f065d870791a1fd03fce0740e64
Abed3228 <Abed3228@gmail.com>
2018-11-02 12:11:04 +0200

    Coupons view model controller

--- FoodOrders/Data/ApplicationDbContext.cs
+++ FoodOrders/Data/ApplicationDbContext.cs
@@ -18,7 +18,7 @@ namespace FoodOrders.Data
         public DbSet<Category> Category { get; set; }
         public DbSet<SubCategory> SubCategory { get; set; }
         public DbSet<MenuItem> MenuItem { get; set; }
-
+        public DbSet<Coupons> Coupons { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder builder)
--- FoodOrders/Data/Migrations/20181101170501_CouponsModel.Designer.cs
+++ FoodOrders/Data/Migrations/20181101170501_CouponsModel.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace FoodOrders.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20181101170501_CouponsModel")]
+    partial class CouponsModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
